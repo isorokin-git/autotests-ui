621f5da21032a4ed9a8cbeafddda1ee209d382e6
isorokin-git <williss012@hotmail.com>
2026-05-13 00:19:07 +0300

    Параметризация Pytest. Практика работы с pytest параметризацией

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -3,12 +3,14 @@ import pytest
 
 @pytest.mark.authorization
 @pytest.mark.regression
-@pytest.mark.parametrize('email, password',
-                         [("user.name@gmail.com", "password"),
-                          ("user.name@gmail.com", "  " ),
-                          ("  ", "password")
-                          ]
-                         )
+@pytest.mark.parametrize(
+    'email, password',
+    [
+        ("user.name@gmail.com", "password"),
+        ("user.name@gmail.com", "  " ),
+        ("  ", "password")
+    ]
+)
 def test_wrong_email_or_password_authorization(chromium_page: Page, email: str, password: str):
     chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
     email_input = chromium_page.get_by_test_id('login-form-email-input').locator('input')
